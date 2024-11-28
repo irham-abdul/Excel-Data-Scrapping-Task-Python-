4aafb84f7047576c19dff2dd4e04e186614a0d57
Iztfq <qazoux@gmail.com>
2024-11-28 11:32:09 +0800

    branch izz

--- TASK3.py
+++ TASK3.py
@@ -6,6 +6,7 @@ import os
 #intersection comments
 #willy willy
 #another intersection comment
+#comment for branching
 print("try")
 def extract_and_append_rows(source_file, target_file, source_sheet_name, target_sheet_name, source_row_start_index, target_row_start, target_column):
     try:
